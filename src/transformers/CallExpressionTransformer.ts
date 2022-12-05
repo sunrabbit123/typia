d97504f228a96b9b9a16c2ffc2a3f5a0c563e9f1
Jeongho Nam <samchon@archisketch.cmo>
2022-12-05 16:28:31 +0900

    Close #392 and Close #393 - remove `clone()` and `createObject()` functions

--- src/transformers/CallExpressionTransformer.ts
+++ src/transformers/CallExpressionTransformer.ts
@@ -2,16 +2,7 @@ import path from "path";
 import ts from "typescript";
 
 import { IProject } from "./IProject";
-import { AssertCloneTransformer } from "./features/cloners/AssertCloneTransformer";
-import { CloneTransformer } from "./features/cloners/CloneTransformer";
-import { CreateAssertCloneTransformer } from "./features/cloners/CreateAssertCloneTransformer";
-import { CreateCloneTransformer } from "./features/cloners/CreateCloneTransformer";
-import { CreateIsCloneTransformer } from "./features/cloners/CreateIsCloneTransformer";
-import { CreateValidateCloneTransformer } from "./features/cloners/CreateValidateCloneTransformer";
-import { IsCloneTransformer } from "./features/cloners/IsCloneTransformer";
-import { ValidateCloneTransformer } from "./features/cloners/ValidateCloneTransformer";
 import { ApplicationTransformer } from "./features/miscellaneous/ApplicationTransformer";
-import { CreateInstanceTransformer } from "./features/miscellaneous/CreateInstanceTransformer";
 import { MetadataTransformer } from "./features/miscellaneous/MetadataTransformer";
 import { AssertParseTransformer } from "./features/parsers/AssertParseTransformer";
 import { CreateAssertParseTransformer } from "./features/parsers/CreateAssertParseTransformer";
@@ -112,15 +103,6 @@ const FUNCTORS: Record<string, () => Task> = {
     isStringify: () => IsStringifyTransformer.transform,
     validateStringify: () => ValidateStringifyTransformer.transform,
 
-    // CLONE FUNCTIONS
-    clone: () => CloneTransformer.transform,
-    assertClone: () => AssertCloneTransformer.transform,
-    isClone: () => IsCloneTransformer.transform,
-    validateClone: () => ValidateCloneTransformer.transform,
-
-    // MISC
-    metadata: () => MetadataTransformer.transform,
-
     //----
     // FACTORY FUNCTIONS
     //----
@@ -142,12 +124,8 @@ const FUNCTORS: Record<string, () => Task> = {
     createIsStringify: () => CreateIsStringifyTransformer.transform,
     createValidateStringify: () => CreateValidateStringifyTransformer.transform,
 
-    // CLONE
-    createClone: () => CreateCloneTransformer.transform,
-    createAssertClone: () => CreateAssertCloneTransformer.transform,
-    createIsClone: () => CreateIsCloneTransformer.transform,
-    createValidateClone: () => CreateValidateCloneTransformer.transform,
-
+    //----
     // MISC
-    createObject: () => CreateInstanceTransformer.transform,
+    //----
+    metadata: () => MetadataTransformer.transform,
 };
